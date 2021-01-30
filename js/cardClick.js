a8eea612e0dddb859139cc053d3730343f2de187
kevin sauvage <kevinsauvage@outlook.com>
2021-01-30 11:57:43 +0100

    My first commit

---
+++ js/cardClick.js
@@ -0,0 +1,20 @@
+const btns = document.querySelectorAll(".about__team-btn");
+const cardHover = document.querySelectorAll(".about__team-card-hover");
+
+function chngIcon(el) {
+  if (el.src.split("/")[5] == "icon-cross.svg") {
+    el.src = "../assets/images/icon-close.svg";
+  } else {
+    el.src = "../assets/images/icon-cross.svg";
+  }
+}
+
+const clickHandler = (e) => {
+  e.currentTarget.nextElementSibling.classList.toggle("appear");
+  var el = e.currentTarget.children[0];
+  chngIcon(el);
+};
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", clickHandler);
+});
---
+++ js/formValidation.js
@@ -0,0 +1,45 @@
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+function ValidateForm(e) {
+  var name = document.forms["Form"]["Name"];
+  var email = document.forms["Form"]["EMail"];
+  var message = document.forms["Form"]["Message"];
+
+  if (name.value == "") {
+    name.style.borderBottom = "1px solid red";
+    name.focus();
+    return false;
+  }
+
+  if (email.value == "" || !validateEmail(email.value)) {
+    email.style.borderBottom = "1px solid red";
+    name.style.borderBottom = "1px solid white";
+    email.focus();
+    return false;
+  }
+
+  if (message.value == "") {
+    email.style.borderBottom = "1px solid white";
+    message.style.borderBottom = "1px solid red";
+    message.focus();
+    return false;
+  }
+
+  return true;
+}
+
+const btn = document.querySelector(".submit-form-btn");
+btn ? btn.addEventListener("click", (e) => ValidateForm(e)) : null;
+
+document.addEventListener(
+  "invalid",
+  (function () {
+    return function (e) {
+      e.preventDefault();
+    };
+  })(),
+  true
+);
