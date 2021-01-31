eb43f7e1b4b6c34aecfeb82087393723f6eddb81
kevin sauvage <kevinsauvage@outlook.com>
2021-01-31 17:57:38 +0100

    adding media queries for mobile

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,13 +1,13 @@
 const hamburger = document.querySelector(".hamburger__menu");
-console.log(hamburger);
+const closeIcon = document.querySelector(".icon__hamburger-menu-close");
+const header = document.querySelector("header");
 
 const openMenuHandler = () => {
-  const header = document.querySelector("header");
-  if (header.style.right != "0px") {
-    header.style.right = "0px";
-  } else {
-    header.style.right = "-101%";
-  }
+  header.style.right = "0px";
+};
+const closeMenuHandler = () => {
+  header.style.right = "-101%";
 };
 
 hamburger.addEventListener("click", openMenuHandler);
+closeIcon.addEventListener("click", closeMenuHandler);
