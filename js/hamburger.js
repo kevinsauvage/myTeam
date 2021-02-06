863fb5cc1a7a50b6bc728233286fc83f1e0ecf19
kevin sauvage <kevinsauvage@outlook.com>
2021-02-06 18:08:56 +0100

    adding alt element to images &  refactoring menu opening handler

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,12 +2,9 @@ const hamburger = document.querySelector(".hamburger__menu");
 const closeIcon = document.querySelector(".icon__hamburger-menu-close");
 const header = document.querySelector("header");
 
-const openMenuHandler = () => {
-  header.style.right = "0px";
-};
-const closeMenuHandler = () => {
-  header.style.right = "-101%";
+const toggleMenuHandler = () => {
+  header.classList.toggle("menu__open");
 };
 
-hamburger.addEventListener("click", openMenuHandler);
-closeIcon.addEventListener("click", closeMenuHandler);
+hamburger.addEventListener("click", toggleMenuHandler);
+closeIcon.addEventListener("click", toggleMenuHandler);
